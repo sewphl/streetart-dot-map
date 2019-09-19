8468973061262455e05df4c8257fc221d8e9d700
sewphl <sharonoptsout@gmail.com>
2019-09-19 16:55:29 -0400

    update gemfile, TweetCreator

--- tweet-api/app/services/tweet_creator.rb
+++ tweet-api/app/services/tweet_creator.rb
@@ -1,9 +1,10 @@
 require 'twitter'
 require 'date'
 require 'pry'
-require 'dotenv/load'
+require 'dotenv-rails'
 
 class TweetCreator
+
   attr_accessor :text, :user_name, :user_id, :timestamp, :year, :month, :day
 
   def initalize(text, user_name, user_id, timestamp, year, month, day)
@@ -16,7 +17,8 @@ class TweetCreator
     @day = day
   end
 
-  def get_tweet()
+  def get_tweet
+    ##binding.pry
     while true
     begin
         config = {
@@ -25,22 +27,25 @@ class TweetCreator
           access_token: ENV['ACCESS_TOKEN'],
           access_token_secret: ENV['ACCESS_TOKEN_SECRET']
         }
-
+        ##binding.pry
         rClient = Twitter::REST::Client.new(config)
         sClient = Twitter::Streaming::Client.new(config)
+        bearer_token = rClient.token
         # topics to watch
         topics = ['streetart']
+        binding.pry
         sClient.filter(:track => topics.join(',')) do |tweet|
             ##puts "whee"
             if (tweet.is_a?(Twitter::Tweet) )  ##&& tweet.uris.length>0 && tweet.geo?
               ##puts "whee2"
-              #binding.pry
-              #Tweet.create!(:text => tweet.text,
-              #:user_name => tweet.user.name, :user_id => tweet.user.id,
-              #:timestamp => tweet.created_at, :year => tweet.created_at.year,
-              #:month => tweet.created_at.month, :day => tweet.created_at.day)
               ##binding.pry
-              sClient.update(@tweet)
+              ##Tweet.create!(:text => tweet.text,
+              ##:user_name => tweet.user.name, :user_id => tweet.user.id,
+              ##:timestamp => tweet.created_at, :year => tweet.created_at.year,
+              ##:month => tweet.created_at.month, :day => tweet.created_at.day)
+              ##binding.pry
+              ##sClient.update(:text => tweet.text)
+
             end
         end
     rescue Exception => e
