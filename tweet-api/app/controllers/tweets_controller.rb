270eb9b804d3d2bc9a07ea99d633e24574393f7c
sewphl <sharonoptsout@gmail.com>
2019-09-19 20:11:22 -0400

    remove non-rails leftovers

--- tweet-api/app/controllers/tweets_controller.rb
+++ tweet-api/app/controllers/tweets_controller.rb
@@ -3,8 +3,16 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
+  def new
+  end
+
   def create
-    @tweet = Tweet.create params[:tweet]
-    redirect_to tweets_path
+    @tweet = Tweet.create(tweet_params) 
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:text,:user_name,:user_id,:timestamp,:year,:month,:day)
   end
 end
