ae3b755f7eb4fe4907c464ae62eab5f7294fe0bf
sewphl <sharonoptsout@gmail.com>
2019-09-20 10:59:05 -0400

    add requirement that tweet have url and lat/lon, update scripts accordingly

--- tweet-api/app/services/tweet_creator.rb
+++ tweet-api/app/services/tweet_creator.rb
@@ -20,8 +20,13 @@ class TweetCreator
         # topics to watch
         topics = ['streetart']
         sClient.filter(:track => topics.join(',')) do |tweet|
-            if (tweet.is_a?(Twitter::Tweet) && tweet.uris.length>0 && tweet.geo?)  ##
-              Tweet.create!(:text => tweet.text,:user_name => tweet.user.name, :user_id => tweet.user.id,:timestamp => tweet.created_at, :year => tweet.created_at.year,:month => tweet.created_at.month, :day => tweet.created_at.day)
+            puts "in the loop"
+            if (tweet.is_a?(Twitter::Tweet) && tweet.uris.length>0 && tweet.geo?)  ##&& tweet.uris.length>0 && tweet.geo?
+              Tweet.create(:text => tweet.text,:user_name => tweet.user.name,
+                :user_id => tweet.user.id,:timestamp => tweet.created_at,
+                :year => tweet.created_at.year,:month => tweet.created_at.month,
+                :day => tweet.created_at.day, :url => tweet.uris[0].display_url,
+                :lon => tweet.geo.coordinates[0], :lat => tweet.geo.coordinates[1])
               puts tweet.text
             end
         end
