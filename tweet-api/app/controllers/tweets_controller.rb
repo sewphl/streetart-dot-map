84d5581229dc1c63ae2a377f7d6cec0ef6d5c1f9
sewphl <sharonoptsout@gmail.com>
2019-09-20 10:59:05 -0400

    add requirement that tweet have url and lat/lon, update scripts accordingly

--- tweet-api/app/controllers/tweets_controller.rb
+++ tweet-api/app/controllers/tweets_controller.rb
@@ -7,12 +7,12 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.create(tweet_params) 
+    @tweet = Tweet.create(tweet_params)
   end
 
   private
 
   def tweet_params
-    params.require(:tweet).permit(:text,:user_name,:user_id,:timestamp,:year,:month,:day)
+    params.require(:tweet).permit(:text,:user_name,:user_id,:timestamp,:year,:month,:day,:url,:lon,:lat)
   end
 end
