c752024ac2b7b4f4ff21bb6df3d742d5124d03d8
sewphl <sharonoptsout@gmail.com>
2019-09-22 17:52:33 -0400

    update AR associations, users and tweets

--- tweet-api/app/models/tweet.rb
+++ tweet-api/app/models/tweet.rb
@@ -1,3 +1,4 @@
 class Tweet < ApplicationRecord
-
+  has_many :tweets_users
+  has_many :users, through: :tweets_users
 end
--- tweet-api/app/models/user.rb
+++ tweet-api/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :tweets_users
   has_many :tweets, through: :tweets_users
 end
