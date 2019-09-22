e70c61f7edda11fb142c8c20dc89acfac2290765
sewphl <sharonoptsout@gmail.com>
2019-09-22 18:39:23 -0400

    fix AR association naming issues

--- tweet-api/app/models/user.rb
+++ tweet-api/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :user_tweets
-  has_many :tweets, through: :user_tweets
+  has_many :users_tweets
+  has_many :tweets, through: :users_tweets
 end
