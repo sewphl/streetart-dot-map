406cdd7f1cf1708f39f60e9560a3fdf3d8378796
sewphl <sharonoptsout@gmail.com>
2019-09-22 17:30:34 -0400

    update users_controller

---
+++ tweet-api/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  #GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  #GET /users/1
+  #def show
+  #  render json: @user
+  #end
+
+end
