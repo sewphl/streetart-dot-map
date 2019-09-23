d73f840e035221b88ccd6d1b8f12f3ebecf4ae31
sewphl <sharonoptsout@gmail.com>
2019-09-23 11:05:03 -0400

    add mock logged in functionality in application controller

--- tweet-api/app/controllers/application_controller.rb
+++ tweet-api/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
+  helpers do
+    def current_user
+      ##mock version of being logged in.
+      User.first
+    end
 
+    def logged_in?
+      !!current_user
+    end
+  end
 end
