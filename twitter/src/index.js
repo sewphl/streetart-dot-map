21e76fbe21b286109d79b614978bb36923202b1f
sewphl <sharonoptsout@gmail.com>
2019-09-25 12:01:27 -0400

    add login functionality

--- twitter/src/index.js
+++ twitter/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import store from './store'
 
