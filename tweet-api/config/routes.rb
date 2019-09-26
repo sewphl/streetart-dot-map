21a9a09e8f6d2091c8ba9b14aedcd6f5f261acfa
sewphl <sharonoptsout@gmail.com>
2019-09-26 14:10:30 -0400

    add logout route

--- tweet-api/config/routes.rb
+++ tweet-api/config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-
-##  controller 'api/v1/login' do
-##    match '*unmatched_route', :to => 'api/v1/sessions#route_options', via: [:options]
-##  end
-##  controller 'api/v1/get_current_user' do
-##    match '*unmatched_route', :to => 'api/v1/sessions#route_options', via: [:options]
-##  end
-
-  ##get "*all" => "sessions#cors_preflight_check", :constraints => { :method => "OPTIONS" }
-
   post "/api/v1/login", to: "api/v1/sessions#create"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   namespace :api do
 	   namespace :v1 do
 	      resources :users
