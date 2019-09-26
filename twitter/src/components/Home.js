382dd0309793b6c25aa1ef2eca37a245db3ca62e
sewphl <sharonoptsout@gmail.com>
2019-09-26 12:56:32 -0400

    add map component

--- twitter/src/components/Home.js
+++ twitter/src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Script from 'react-load-script'
+import scriptLoader from 'react-async-script-loader'
 
 export default class Home extends React.Component {
-render() {
-  return (
-    <div>
-    <h1>home</h1>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div>
+      <h1>home</h1>
+      </div>
+    );
+  }
 }
