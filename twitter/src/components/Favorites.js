9aad64f9a223a34835b21fecae37c5653945068c
sewphl <sharonoptsout@gmail.com>
2019-09-27 15:36:16 -0400

    update favorites component to yield username

--- twitter/src/components/Favorites.js
+++ twitter/src/components/Favorites.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import Logout from './Logout'
+import Login from './Login'
 
-export default class Favorites extends React.Component {
-  render() {
+const Favorites = ({currentUser, loggedIn}) => {
+  //render() {
     return (
       <div>
-        <h1>favorites</h1>
+        <h1>{currentUser.username} favorites</h1>
         </div>
       );
-    }
+  //  }
 }
+
+const mapStateToProps = ({currentUser}) => {
+  return {
+    currentUser,
+    loggedIn: !!currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Favorites)
