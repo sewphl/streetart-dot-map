003de3f6220e97de91c8e5bb992c4e87c3ed34c5
sewphl <sharonoptsout@gmail.com>
2019-10-18 13:59:30 -0400

    update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191008190154) do
+ActiveRecord::Schema.define(version: 20191018173248) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "tweets", force: :cascade do |t|
     t.string   "text"
