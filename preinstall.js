a9e132c15624d1cd6213f817d076fc3262bf7254
sewphl <sharonoptsout@gmail.com>
2019-10-24 19:06:57 -0400

    update preinstall.js

--- preinstall.js
+++ preinstall.js
@@ -1,2 +1,1 @@
-const fs=require('fs');
-fs.writeFile(process.env.GOOGLE_APPLICATION_CREDENTIALS, process.env.GOOGLE_CONFIG, (err) => {});
+const fs=require('fs'); fs.writeFile('./google-credentials-heroku.json', process.env.GOOGLE_CONFIG, (err) => {});
