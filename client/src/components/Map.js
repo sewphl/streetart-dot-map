3766b184852c0ce01d03f4128af251a5a5b8f9ab
sewphl <sharonoptsout@gmail.com>
2019-10-25 15:09:59 -0400

    update map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -8,27 +8,6 @@ import { favorite, updateFavoriteForm } from "../actions/favorites"
 import InfoWindowEx from './InfoWindowEx'
 //import InfoWindowEx from './InfoWindow2'
 
-
-const {auth} = require('google-auth-library');
-
-// load the environment variable with our keys
-const keysEnvVar = process.env['CREDS'];
-if (!keysEnvVar) {
-  throw new Error('The $CREDS environment variable was not found!');
-}
-const keys = JSON.parse(keysEnvVar);
-
-async function main() {
-  // load the JWT or UserRefreshClient from the keys
-  const client = auth.fromJSON(keys);
-  client.scopes = ['https://www.googleapis.com/auth/cloud-platform'];
-  const url = `https://dns.googleapis.com/dns/v1/projects/${keys.project_id}`;
-  const res = await client.request({url});
-  console.log(res.data);
-}
-
-main().catch(console.error);
-
 const mapStyles = {
   margin: 'auto',
   width: '70%',
